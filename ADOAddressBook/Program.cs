099255251a730688074576840d8ece8ee553e820
ALLTALE2027 <86816300+ALLTALE2027@users.noreply.github.com>
2022-10-02 02:37:42 +0530

    [Darshan]: display contacts from address book DB

---
+++ ADOAddressBook/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace ADOAddressBook
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Welcome to contact management program");
+            AddressBookADO Book = new AddressBookADO();
+
+            ShowOptions();
+
+            void ShowOptions()
+            {
+                Console.WriteLine("\n Select Option :\n1.Display Contacts\n4.Exit");
+                int option = int.Parse(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        Book.GetContactDetails();
+                        ShowOptions();
+                        break;
+                    default:
+                        Console.WriteLine("Enter correct choice");
+                        break;
+                }
+            }       
+        }
+        
+    }
+
+}
